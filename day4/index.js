36f0260832d8dc2e771368a2b0166bd23c3c0e01
Mikael Malm <mikaelmalm91@gmail.com>
2020-12-04 11:01:36 +0100

    Day 4 solution 2

--- day4/index.js
+++ day4/index.js
@@ -18,15 +18,77 @@ const readFile = () =>
 
     .catch((err) => console.log(err));
 
+const getPassportFields = (passport) =>
+  passport.reduce((result, pass) => [...result, pass[0]], []);
+
+const validateRange = (value, min, max) => value >= min && value <= max;
+const validateYear = (year, min, max) =>
+  year.length === 4 && validateRange(year, min, max);
+
+const validateHeight = (rawHeight) => {
+  const height = rawHeight.split(/[a-z]/).filter((value) => value)[0];
+  const format = rawHeight.split(/[0-9]/).filter((value) => value)[0];
+
+  const cmValidation = { min: 150, max: 193 };
+  const inValidation = { min: 59, max: 76 };
+
+  const validation = format === "cm" ? cmValidation : inValidation;
+
+  return height >= validation.min && height <= validation.max;
+};
+
+const validateHairColor = (color) => /^#[a-f0-9]{6}$/.test(color);
+const validateEyeColor = (color) => {
+  const acceptedColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+
+  return acceptedColors.includes(color);
+};
+
+const validatePassportId = (id) => {
+  const numbers = id.match(/[0-9]/g);
+
+  return id.length === 9 && numbers.length === 9;
+};
+
+const validateData = (data) => {
+  const [key, value] = data;
+
+  switch (key) {
+    case "byr":
+      return validateYear(value, 1920, 2002);
+
+    case "iyr":
+      return validateYear(value, 2010, 2020);
+
+    case "eyr":
+      return validateYear(value, 2020, 2030);
+
+    case "hgt":
+      return validateHeight(value);
+
+    case "hcl":
+      return validateHairColor(value);
+
+    case "ecl":
+      return validateEyeColor(value);
+
+    case "pid":
+      return validatePassportId(value);
+
+    case "cid":
+      // hackerman
+      return true;
+
+    default:
+      return false;
+  }
+};
+
 const checkPassports = (passports) => {
-  // since 'cid' was not required, we don't even think about it
   const requiredKeys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
-  return passports.filter((passport) => {
-    const passportFields = passport.reduce(
-      (result, pass) => [...result, pass[0]],
-      []
-    );
+  const passportsWithAllFields = passports.filter((passport) => {
+    const passportFields = getPassportFields(passport);
 
     const allKeysPresent = requiredKeys.every((key) =>
       passportFields.includes(key)
@@ -34,6 +96,14 @@ const checkPassports = (passports) => {
 
     return allKeysPresent;
   });
+
+  const validatedPassport = passportsWithAllFields.filter((passport, index) => {
+    const validated = passport.every((pass) => validateData(pass));
+
+    return validated;
+  });
+
+  return validatedPassport;
 };
 
 const run = async () => {
