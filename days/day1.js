92cacad12569cd0076912ebdcc80d93f04cd465f
Mikael Malm <mikaelmalm91@gmail.com>
2020-12-06 19:55:52 +0100

    refactor: day 1

---
+++ days/day1.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+
+const readFile = () =>
+  fs.promises
+    .readFile(`${__dirname}/data.txt`, 'utf8')
+    .then((res) => res.split('\r\n'))
+    .then((res) => res.map((field) => +field))
+    .catch((err) => console.log(err));
+
+const parseData = (data) => data.split('\r\n').map((field) => +field);
+
+const multiplyItems = (items) =>
+  items.reduce((sum, item) => sum * item, 1);
+
+const getValues = (data, compareArr = data) => {
+  for (let i = 0; i < data.length; i += 1) {
+    const item = data[i];
+    const sumNeeded = 2020 - item;
+
+    const outcome = compareArr.find((value) => {
+      if (typeof value === 'number') {
+        return value === sumNeeded;
+      }
+
+      return value.sumOfItems === sumNeeded;
+    });
+
+    if (outcome) {
+      return typeof outcome === 'number'
+        ? [sumNeeded, item]
+        : [...outcome.values, item];
+    }
+  }
+
+  return [];
+};
+
+const calculateOutcomes = (input) => {
+  const outcomesArr = input.map((value, index) => {
+    const complareArr = input.slice(index);
+
+    const valueOutcomes = complareArr
+      .reduce((sum, compareValue) => {
+        const sumOfItems = compareValue + value;
+
+        return [
+          ...sum,
+          { sumOfItems, values: [compareValue, value] },
+        ];
+      }, [])
+      .filter((currentObj) => currentObj.sumOfItems < 2020);
+
+    return valueOutcomes;
+  });
+
+  return outcomesArr.flat();
+};
+
+const solution1 = (data) => {
+  const parsedData = parseData(data);
+  const numbersForSum = getValues(parsedData);
+  const result = multiplyItems(numbersForSum);
+
+  return result;
+};
+
+const solution2 = (data) => {
+  const parsedData = parseData(data);
+
+  const possibleOutcomes = calculateOutcomes(parsedData);
+  const matchingValues = getValues(parsedData, possibleOutcomes);
+  const result = multiplyItems(matchingValues);
+
+  return result;
+};
+
+export { solution1, solution2 };
---
+++ tests/day1/day1.test.js
@@ -0,0 +1,25 @@
+import { readFile } from '../../utils';
+
+import { solution1, solution2 } from '../../days/day1';
+
+describe('[01]', () => {
+  it('[Part 1] - should work with real data', async () => {
+    const input = await readFile(`${__dirname}/data.txt`);
+
+    // we got this by failing the test the first time
+    const expectedAnswer = 1010299;
+    const actualAnswer = solution1(input);
+
+    expect(actualAnswer).toEqual(expectedAnswer);
+  });
+
+  it('[Part 2] - should work with real data', async () => {
+    const input = await readFile(`${__dirname}/data.txt`);
+
+    // we got this by failing the test the first time
+    const expectedAnswer = 42140160;
+    const actualAnswer = solution2(input);
+
+    expect(actualAnswer).toEqual(expectedAnswer);
+  });
+});
